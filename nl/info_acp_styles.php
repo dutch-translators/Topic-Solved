a321d7d97ba8221e9114e564de852f10302a71ec
David <neurofibromatose@live.nl>
2015-02-19 11:29:27 +0100

    eerste vertaling

--- nl/info_acp_styles.php
+++ nl/info_acp_styles.php
@@ -4,6 +4,7 @@
  *
  * @copyright (c) Bryan Petty
  * @license GNU General Public License, version 2 (GPL-2.0)
+ * [Dutch] translated by Dutch Translators (https://github.com/dutch-translators)
  *
  * @package tierra/topicsolved/language
  */
@@ -19,9 +20,9 @@ if (empty($lang) || !is_array($lang))
 }
 
 $lang = array_merge($lang, array(
-	'IMG_ICON_TOPIC_SOLVED_HEAD'    => 'Topic is solved',
-	'IMG_ICON_TOPIC_SOLVED_LIST'    => 'Topic is solved',
-	'IMG_ICON_TOPIC_SOLVED_POST'    => 'Topic is solved',
-	'IMG_ICON_TOPIC_SOLVED_SET'     => 'Accept this answer',
-	'IMG_ICON_TOPIC_SOLVED_UNSET'   => 'Mark topic as not solved',
+	'IMG_ICON_TOPIC_SOLVED_HEAD'    => 'Onderwerp is opgelost',
+	'IMG_ICON_TOPIC_SOLVED_LIST'    => 'Onderwerp is opgelost',
+	'IMG_ICON_TOPIC_SOLVED_POST'    => 'Onderwerp is opgelost',
+	'IMG_ICON_TOPIC_SOLVED_SET'     => 'Accepteer dit antwoord',
+	'IMG_ICON_TOPIC_SOLVED_UNSET'   => 'Markeer topic als onopgelost',
 ));
